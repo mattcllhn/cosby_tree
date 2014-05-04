a4d867cdf5b014ea47d7afdcc43dbc276da598aa
matt <mattcllhn@gmail.com>
2014-05-04 12:40:41 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,24 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CosbyTree::Application.config.secret_key_base = '8715947b11af7238384b657d16cf7b8213b1d9878051eb9c9bac1002a12608143d9f28978734a178b7749ad8df2cade2e55d77ca07261acf086fdb2cbf4127c7'
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
